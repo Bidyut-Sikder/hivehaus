5f96eb0a8e44b21ebd779d32b8fed13e1801944a
bidyut-sikder <bidyutsikder2001@gmail.com>
2025-01-20 22:09:44 +0600

    adding room apis callings

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,27 +1,78 @@
-import { TRoomsResponse } from "@/types/global";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { TRoomsResponse, TSingleRoomResponse } from '@/types/global';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/" }),
- 
-  tagTypes: ["Slots", "Bookings"],
-  endpoints: (builder) => ({
-    getRooms: builder.query({
-      query: ({ search, sortBy }) => {
-        return {
-          url: "/rooms",
-          params: { search, sort: sortBy },
-        };
-      },
-      transformResponse: (response: TRoomsResponse) => response.data,
-      providesTags: ["Slots"],
-    }),
-  }),
-});
+    reducerPath: 'baseApi',
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/" }),
+    tagTypes: ['Slots', 'Bookings'],
+    endpoints: (builder) => ({
+        getRooms: builder.query({
+            query: ({ search, sortBy }) => {
+                return {
+                    url: '/rooms',
+                    params: { search, sort: sortBy }
+                };
+            },
+            transformResponse: (response: TRoomsResponse) => response.data,
+            providesTags: ['Slots']
+        }),
+        getSingleRoom: builder.query({
+            query: (id) => {
+                return {
+                    url: `/rooms/${id}`
+                }
+            },
+            transformResponse: (response: TSingleRoomResponse) => response.data,
+            providesTags: ['Slots']
+        }),
+        createRoom: builder.mutation({
+            query: ({ roomData, token }) => {
+                console.log(roomData);
+                return {
+                    url: '/rooms',
+                    method: 'POST',
+                    body: roomData,
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            },
+        }),
+        updateRoom: builder.mutation({
+            query: ({ updatedData, token, roomId }) => {
+                console.log(updatedData);
+                return {
+                    url: `/rooms/${roomId}`,
+                    method: 'PATCH',
+                    body: updatedData,
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            },
+            invalidatesTags: ['Slots']
+        }),
+        deleteRoom: builder.mutation({
+            query: ({ token, roomId }) => {
+                return {
+                    url: `/rooms/${roomId}`,
+                    method: 'DELETE',
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                }
+            },
+            invalidatesTags: ['Slots']
+        }),
+    })
+})
 
 export const {
-  useGetRoomsQuery,
-
-  useLazyGetRoomsQuery,
-} = baseApi;
+    useGetRoomsQuery,
+    useGetSingleRoomQuery,
+    useCreateRoomMutation,
+    useUpdateRoomMutation,
+    useDeleteRoomMutation,
+    useLazyGetRoomsQuery
+} = baseApi
